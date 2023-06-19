7cdf4fbd1336fd4b1bcb8bcb7722d4923fbfb6f7
Jason <jtsu@cisco.com>
2023-06-19 15:42:20 -0700

    edit reboot script

--- merakiScripts/5_rebootDevices.py
+++ merakiScripts/5_rebootDevices.py
@@ -1,39 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Copyright (c) 2021 Cisco and/or its affiliates.
-
-This software is licensed to you under the terms of the Cisco Sample
-Code License, Version 1.1 (the "License"). You may obtain a copy of the
-License at
-               https://developer.cisco.com/docs/licenses
-
-All use of the material herein must be in accordance with the terms of
-the License. All rights not expressly granted by the License are
-reserved. Unless required by applicable law or agreed to separately in
-writing, software distributed under the License is distributed on an "AS
-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-or implied.
-"""
 
 import json
 import meraki
-from webexteamssdk import WebexTeamsAPI
-import time
 import tokens
-import csv
 
 dashboard = meraki.DashboardAPI(tokens.API_KEY, suppress_logging=True)
 delay = 2
-
-
-def readCsvFile():
-    with open('/scripts/apSerials.csv', 'r') as file:
-        read = csv.reader(file)
-        csvRows = []
-        for row in read:
-            csvRows.append(row)
-    return (csvRows)
 
 
 def getDeviceName(deviceSerialNumbers):
@@ -70,14 +41,6 @@ def rebootDevice(deviceSerialNumbers):
     return(rebootStatus)
 
 
-def postWebex(rebootStatus):
-    # Using the Webex SDK. More info can be found here: https://webexteamssdk.readthedocs.io/en/latest/index.html
-    webex = WebexTeamsAPI(access_token=tokens.WEBEX_TOKEN)
-
-    # Post the rebootStatus to the webex room
-    webex.messages.create(tokens.WEBEX_ROOMID, markdown=json.dumps(rebootStatus))
-    print("Reboot status results posted to Webex room.")
-
 
 def rebootStatus(rebootResults):
     passCount = 0
@@ -99,15 +62,9 @@ def rebootStatus(rebootResults):
 
 if __name__ == '__main__':
 
-    # Get list of device serial numbers to reboot from CSV file.
-    rows = readCsvFile()
+
     deviceSerialNumbers = []
-    for row in range(len(rows)):
-        # nested loops in case of multiple rows in CSV file
-        for serial in rows[row]:
-            deviceSerialNumbers.append(serial)
 
     print("Starting reboot script.  It may take several minutes to complete.")
     rebootAP = rebootDevice(deviceSerialNumbers)
     status = rebootStatus(rebootAP)
-    postWebex(status)
