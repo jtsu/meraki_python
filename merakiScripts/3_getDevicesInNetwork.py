0d84256fb00fdd3bc649a8e276c00fddf25a1f9b
Jason <jtsu@cisco.com>
2023-06-20 11:20:57 -0700

    update scripts

--- merakiScripts/3_getDevicesInNetwork.py
+++ merakiScripts/3_getDevicesInNetwork.py
@@ -2,9 +2,9 @@
 # Meraki API Reference:
 # https://developer.cisco.com/meraki/api-latest/#!list-the-devices-in-a-network
 
-
 import tokens
 import requests
+import json
 
 base_url = "https://api.meraki.com/api/v1"
 resource_path = f"/networks/{tokens.NET_A_ID}/devices"
@@ -19,5 +19,6 @@ headers = {
 
 response = requests.request('GET', url, headers=headers, data = payload)
 
-print(response.text.encode('utf8'))
-
+#print(response.text.encode('utf8'))
+json_data = response.json()
+print(json.dumps(json_data, indent=2))
