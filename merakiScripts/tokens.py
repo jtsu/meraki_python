2acedafabdcf8215e422e41aab2ca607b7a9d480
Jason <jtsu@cisco.com>
2023-06-19 19:01:45 -0700

    Update tokens.py

--- merakiScripts/tokens.py
+++ merakiScripts/tokens.py
@@ -1,3 +1,8 @@
+# Defining your API key as a variable in source code is not recommended
+# Instead, use an environment variable as shown under the Usage section
+# @ https://github.com/meraki/dashboard-api-python/
+
+
 API_KEY = "your api key"
 ORG_ID = 'your org_id'
 
